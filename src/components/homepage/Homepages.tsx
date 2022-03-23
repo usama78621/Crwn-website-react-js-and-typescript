2b8e0de495df38a8a05a8ca1f6314f1c97220694
usama78621 <92777217+usama78621@users.noreply.github.com>
2022-03-23 09:48:08 +0500

    create react app using create react-app and the homepage  first

---
+++ src/components/homepage/Homepages.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "../../components/homepage.scss";
+const Homepages = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="item-menu">
+          <h1 className="heading">Hats</h1>
+          <span className="title">Show Now</span>
+        </div>
+        <div className="item-menu">
+          <h1 className="heading">Hats</h1>
+          <span className="title">Show Now</span>
+        </div>
+        <div className="item-menu">
+          <h1 className="heading">Hats</h1>
+          <span className="title">Show Now</span>
+        </div>
+        <div className="item-menu">
+          <h1 className="heading">Hats</h1>
+          <span className="title">Show Now</span>
+        </div>
+        <div className="item-menu">
+          <h1 className="heading">Hats</h1>
+          <span className="title">Show Now</span>
+        </div>
+        <div className="directory-menu">
+          <div className="item-menu">
+            <h1 className="heading">Hats</h1>
+            <span className="title">Show Now</span>
+          </div>
+          <div className="item-menu">
+            <h1 className="heading">Hats</h1>
+            <span className='title'>Show Now</span>
+          </div>
+          <div className="item-menu">
+            <h1 className="heading">Hats</h1>
+            <span className='title'>Show Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepages;
